156ed18f1bd2e079a77923229da0142427c55440
Cezarrull <cezarrull12345@gmail.com>
2024-11-12 13:52:26 +0000

    :fire: add text and imgs

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,16 +8,12 @@ export function Home() {
             <div className="homeBody">
                 <div className="container">
                     <div className="text">
-
+                        <h2>TESTE SOBRE ALINHAMENTO DE TEXTO PARA DIV</h2>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum omnis exercitationem voluptates perferendis temporibus pariatur, et animi ipsum, accusamus doloremque unde fugit veniam repudiandae, cumque dignissimos rem voluptatem eligendi soluta?
+                        </p>
                     </div>
-                    <div className="imgText">
-                        <div className="textTwo">
-
-                        </div>
-                        <div className="image">
-
-                        </div>
-                    </div>
+                    <div className="image"></div>
                 </div>
             </div>
         </>
